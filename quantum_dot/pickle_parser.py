b11501880c78b7872db79ca863a7dd579d78ec79
danieljamesgraham <139626610+danieljamesgraham@users.noreply.github.com>
2024-07-11 14:37:15 +0100

    Moved DigitalOutput class into PickleParse class.

--- quantum_dot/pickle_parser.py
+++ quantum_dot/pickle_parser.py
@@ -1,28 +1,63 @@
 # TODO: Extract parsing function and inherit it instead.
 
+from qick import QickConfig
+
 class PickleParse():
+    sequence = []
+
     def __init__(self, pulse_sequences):
         # self.soccfg = soccfg
         self.pulse_sequences = pulse_sequences
-
         self.ch_cfg = {}
 
         valid_channels = ["DAC_A", "DAC_B", "PMOD_0", "PMOD_1", "PMOD_2", "PMOD_3"]
 
         for channel, seq_params in self.pulse_sequences.items():
+
+            # Check validity of arguments and extract channel info -------------
+
             if channel not in valid_channels:
                 raise Exception(channel, "is not a valid output")
         
             ch_type = channel.split('_')[0]
             ch_index = channel.split('_')[1]
             
+            # PMOD parsing -----------------------------------------------------
+
             if ch_type == "PMOD":
                 print("----- PMOD ------")
-        
+
+                # Convert list of tuples into list of lists
+                seq_params = [list(t) for t in seq_params]
+
+                # Convert pulse widths into elapsed time and append channel to 
+                # list
+                running_time = 0
+                for l in seq_params:
+                    l[0], running_time = running_time, running_time + l[0]
+                    l.append(ch_index)
+
+                delete_indices = []
+                for i in range(len(seq_params) - 1):
+                    if seq_params[i][1] == seq_params[i+1][1]:
+                        delete_indices.append(i+1)
+                for i in reversed(delete_indices):
+                    del seq_params[i]
+                
+                # End final pulse
+                seq_params.append([running_time, 0, ch_index])
+
+                # Add channel sequence to master sequence and sort in order of 
+                # time
+                [self.sequence.append(l) for l in seq_params]
+                self.sequence.sort(key=lambda x: x[0])
+
+            # DAC parsing ------------------------------------------------------
+
             if ch_type == "DAC":
                 print("----- DAC -----")
 
-                on_durations = []
+                pulse_lens = []
                 on_times = []
                 on_freqs = []
 
@@ -33,7 +68,7 @@ class PickleParse():
 
                 for l in seq_params:
                     if int(l[1]) == 1:
-                        on_durations.append(int(l[0])/1e3)
+                        pulse_lens.append(int(l[0])/1e3)
 
                 running_time = 0
                 for l in seq_params:
@@ -43,31 +78,56 @@ class PickleParse():
                     running_time += l[0]
                 finish_time = running_time/1e3
 
-                if len(on_durations) != len(on_times):
+                if len(pulse_lens) != len(on_times):
                     raise ValueError # TODO: Make error less generic
                 
-                seq_length = len(on_durations)
+                seq_length = len(pulse_lens)
                 # TODO: Actually implement this
                 # TODO: Class-wide variable that stores sequence lengths
 
-                # print(on_durations, on_times, on_freqs, finish_time)
-                self.ch_cfg[channel] = {"durations": on_durations,
+                # print(pulse_lens, on_times, on_freqs, finish_time)
+                self.ch_cfg[channel] = {"pulse_lens": pulse_lens,
                                         "times": on_times,
                                         "freqs": on_freqs,
                                         "finish": finish_time,
-                                        "pulses": seq_length,
-                                        "type": ch_type,
+                                        "num_pulses": seq_length,
+                                        "ch_type": ch_type,
                                         "ch_ref": ch_ref
                                         }
                 
                 for key, value in self.ch_cfg[channel].items():
                     print(key, value)
 
+        # Post-parsing information ---------------------------------------------
+
         last_finish = 0 
         for channel in self.ch_cfg:
             if self.ch_cfg[channel]["finish"] > last_finish:
                 last_finish = self.ch_cfg[channel]["finish"]
         print("Last finish:", last_finish)
+
+# ------------------------------------------------------------------------------
+
+    def configure(self, soccfg):
+        self.soccfg = soccfg
+        
+        out = 0
+        for l in DigitalOutput.sequence: 
+            time = int(QickConfig.us2cycles((l[0]) / 1e3))
+            state = l[1]
+            bit_position = l[2]
+
+            if state == 1:
+                out |= (1 << bit_position)
+            elif state == 0:
+                out &= ~(1 << bit_position)
+
+            rp = 0 # tproc register page
+            r_out = 31 # tproc register
+            # print(bin(out), time)
+            self.regwi(rp, r_out, out)
+            self.seti(soccfg['tprocs'][0]['output_pins'][0][1], rp, r_out, time)
+
 
 
 
