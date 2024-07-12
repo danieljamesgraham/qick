1a69fa25a8ce9b087dd802eae018e65f5ca9f555
danieljamesgraham <139626610+danieljamesgraham@users.noreply.github.com>
2024-07-12 17:41:07 +0100

    Added channel independent gain control.

--- quantum_dot/pickle_parser.py
+++ quantum_dot/pickle_parser.py
@@ -3,9 +3,12 @@ from qick import QickProgram
 
 valid_channels = ["DAC_A", "DAC_B", "DIG_0", "DIG_1", "DIG_2", "DIG_3"]
 
+# TODO: Default constants
+DEFAULT_GAIN = 10000
+
 class PickleParse():
 
-    def __init__(self, imported_seqs, ch_map=None, delays={}):
+    def __init__(self, imported_seqs, ch_map=None, gains={}, delays={}):
         self.ch_cfg = {}
         self.dig_seq = {}
 
@@ -24,11 +27,14 @@ class PickleParse():
             if type(delay) != int:
                 raise Exception(f"{channel} delay ({delay}) must be an integer number of clock cycles")
 
+        # TODO: Gain and amplitude parameter checks (ie. int between 1 and 30000)
+
         for channel, seq_params in self.pulse_seqs.items():
             lengths, times = [], []
             
             # Check channel names are valid
-            # TODO: Times, states, frequencies and phases must be floats or ints
+            # TODO: (?) Times, states, frequencies and phases must be floats or ints
+            # TODO: Specify minimum pulse durations
             if channel not in valid_channels:
                 raise Exception(f"{channel} is not a valid channel. Try any from:\n{valid_channels}")
 
@@ -42,7 +48,7 @@ class PickleParse():
 
             if ch_type == "DAC":
                 print(f"----- DAC {ch_ref} -----")
-                freqs, phases = [], []
+                amps, freqs, phases = [], [], []
                 # Assign correct QICK channel index
                 if ch_ref == 'A':
                     ch_index = 1
@@ -53,18 +59,33 @@ class PickleParse():
                 delay = delays[channel] # Trigger delay [proc. clock cycles]
             else:
                 delay = 0
+            
+            if channel in gains:
+                gain = gains[channel]
+            else:
+                gain = DEFAULT_GAIN
 
             # Create lists of pulse parameters
             time = 0
             for l in seq_params:
-                if bool(l[1]) == True:
+                # Add pulse parameters to respective lists
+                if (bool(l[1]) == False) and (len(l) > 2):
+                    raise Exception(f"Specified too many sequence parameters for pulse in channel {channel}")
+                elif bool(l[1]) == True:
+                    if ((len(l) > 2) and (ch_type == "DIG")
+                        or (len(l) > 4) and (ch_type == "DAC")):
+                        raise Exception(f"Specified too many sequence parameters for pulse in channel {channel}")
+
                     times.append(time/1e3) # Trigger time [us]
                     lengths.append(l[0]/1e3) # Pulse durations [us]
+
                     if ch_type == "DAC":
+                        amps.append(l[1])
                         freqs.append(l[2]*1e3) # DAC frequency [Hz]
                         phases.append(l[3]) # DAC phase [deg]
+
                 time += l[0]
-            finish = time/1e3 # End time of sequence [us]
+            duration = time/1e3 # End time of sequence [us]
 
             # Raise error if pulse parameter lists are not equal in length
             num_pulses = len(lengths)
@@ -76,48 +97,30 @@ class PickleParse():
             self.ch_cfg[channel] = {"ch_type": ch_type,
                                     "ch_index": ch_index,
                                     "num_pulses": num_pulses,
+                                    "duration": duration,
                                     "delay": delay,
+                                    "gain": gain,
                                     "times": times,
                                     "lengths": lengths,
-                                    "finish": finish,
-                                    **({"freqs": freqs,
+                                    **({"amps": amps,
+                                        "freqs": freqs,
                                         "phases": phases}
                                        if ch_type == "DAC" else {})
                                     }
             for key, value in self.ch_cfg[channel].items():
                 print(f"{key}: {value}")
 
-        # Find and print time at which last state in sequence ends
-        self.end_time = 0 
-        for channel in self.ch_cfg:
-            if self.ch_cfg[channel]["finish"] > self.end_time:
-                self.end_time = self.ch_cfg[channel]["finish"]
+        # Find and print time at which longest sequence ends
+        end_times = [self.ch_cfg[channel]["duration"] for channel in self.ch_cfg]
+        self.end_time = max(end_times)
+
         print(f"----- End time: {self.end_time} -----")
+        if not all(i == end_times[0] for i in end_times):
+            print("WARNING! Not all sequences are of the same duration")
 
-
-        # TODO: Raise error if end times don't match
         # TODO: Make sure not all parameters have to be specified for DAC
-        # TODO: Make sure to raise error if too many parameters specified for DIG
-
-
-
-    def dac_defaults(self, prog, gain, freq, phase):
-        ch_cfg = self.ch_cfg
-
-        # TODO: Default gain, frequency and phase if not specified?
-        # Add gain for each pulse?
-        # Add logic to maintain values if not specified for a pulse?
-
-        for channel in ch_cfg:
-            if ch_cfg[channel]["ch_type"] == "DAC":
-                ch_index = ch_cfg[channel]["ch_index"]
-                gain = 10000
-
-                # Initialise DAC
-                prog.declare_gen(ch=ch_index, nqz=1) 
-
-                # Set default pulse parameters
-                prog.default_pulse_registers(ch=ch_index, gain=gain) 
+        # FIXME: Make sure to raise error if too many parameters specified for DIG
+        # TODO: Add check for DAC gain
 
     def generate_asm(self, prog, reps=1):
         ch_cfg = self.ch_cfg
@@ -135,18 +138,28 @@ class PickleParse():
             delay = ch_cfg[channel]["delay"]
 
             if ch_cfg[channel]["ch_type"] == "DAC":
+                prog.declare_gen(ch=ch_index, nqz=1) 
+
                 for i in range(num_pulses):
                     # DAC pulse parameters
-                    freq = prog.freq2reg(ch_cfg[channel]["freqs"][i], 
-                                         gen_ch=ch_index)
-                    phase = prog.deg2reg(ch_cfg[channel]["phases"][i],     
-                                         gen_ch=ch_index)
                     time = prog.us2cycles(ch_cfg[channel]["times"][i]) + delay
                     length = prog.us2cycles(ch_cfg[channel]["lengths"][i], gen_ch=ch_index)
+                    amp = int(ch_cfg[channel]["amps"][i] * ch_cfg[channel]["gain"])
+                    freq = prog.freq2reg(ch_cfg[channel]["freqs"][i], 
+                                         gen_ch=ch_index
+                                         )
+                    phase = prog.deg2reg(ch_cfg[channel]["phases"][i],     
+                                         gen_ch=ch_index
+                                         )
 
                     # Store DAC parameters in register and trigger pulse
-                    prog.set_pulse_registers(ch=ch_index, freq=freq, phase=phase, 
-                                             style="const", length=length)
+                    prog.set_pulse_registers(ch=ch_index,
+                                             gain=amp,
+                                             freq=freq, 
+                                             phase=phase,
+                                             style="const",
+                                             length=length
+                                             )
                     prog.pulse(ch=ch_index, t=time)
 
             if ch_cfg[channel]["ch_type"] == "DIG":
